75a2d0224c67cb09f372816467f7d2fffd2b1348
1huali <wanhuali2000@gmail.com>
2022-01-16 12:28:22 -0500

    -adding sound fx and botSquad pic

--- wheres-sausage-dog/js/script.js
+++ wheres-sausage-dog/js/script.js
@@ -9,8 +9,10 @@ if it can identify the real from the fake dog.
 "use strict";
 // let state = 'main';
 let clicks = 0;
-// let botSquadImg = undefined;
+let botSquadImg = undefined;
+let botSquad = undefined;
 
+let dangerFX = undefined;
 
 const NUM_ANIMAL_IMG = 11;
 const NUM_ANIMAL = 100;
@@ -32,7 +34,8 @@ function preload() {
   }
 
   captchaImg = loadImage(`assets/images/captcha.jpeg`);
-  // botSquadImg = loadImage(`assets/images/bot.jpeg`);
+  botSquadImg = loadImage(`assets/images/bot.jpeg`);
+  dangerFX = loadSound(`assets/sounds/bark.wav`)
 
 }
 
@@ -42,10 +45,11 @@ Create canvas, create animal and captcha objs and stored them into their global 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  window.alert('Click on the authentic dog to testify authentic human identity.         Five (5) trials allowed. BACKSPACE to refresh.');
+  window.alert('Click on the authentic dog to testify your human identity.                  Five (5) trials allowed. BACKSPACE to refresh.');
 
   createAnimals();
   createCaptcha();
+  createBotSquad();
 }
 
 /**
@@ -55,88 +59,100 @@ function draw() {
   background(0);
 
   // if (state === `main`) {
-    updateAnimals();
-    captcha.update();
+  updateAnimals();
+  captcha.update();
+}
+//   // if (state === `gotchu`) {
+//   //   end();
+//   // }}
+//
+// // }
+// }
+
+function mousePressed() {
+  // console.log(mouseX, mouseY);
+  captcha.mousePressed();
+
+  clicks++;
+  console.log(clicks);
+  test();
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    refresh()
   }
-  //   // if (state === `gotchu`) {
-  //   //   end();
-  //   // }}
-  //
-  // // }
-  // }
-
-  function mousePressed() {
-    // console.log(mouseX, mouseY);
-    captcha.mousePressed();
-
-    clicks++;
-    console.log(clicks);
-    test();
-  }
-
-  function keyPressed() {
-    if (keyCode === 32) {
-      refresh()
-    }
-  }
+}
 
 
-  function createAnimals() {
-    for (let i = 0; i < NUM_ANIMAL; i++) {
-      let x = random(0, width);
-      let y = random(0, height);
-      let animalImage = random(animalImgs);
-      let animal = new Animal(x, y, animalImage);
-      animals.push(animal);
-    }
-  }
-
-  function createCaptcha() {
+function createAnimals() {
+  for (let i = 0; i < NUM_ANIMAL; i++) {
     let x = random(0, width);
     let y = random(0, height);
-    captcha = new Captcha(x, y, captchaImg);
-    // console.log(Captcha);
+    let animalImage = random(animalImgs);
+    let animal = new Animal(x, y, animalImage);
+    animals.push(animal);
   }
+}
 
-  function updateAnimals() {
-    for (let i = 0; i < animals.length; i++) {
-      animals[i].update();
-    }
+function createCaptcha() {
+  let x = random(0, width);
+  let y = random(0, height);
+  captcha = new Captcha(x, y, captchaImg);
+  // console.log(Captcha);
+}
+
+function createBotSquad() {
+  botSquadImg = botSquad;
+}
+
+function updateAnimals() {
+  for (let i = 0; i < animals.length; i++) {
+    animals[i].update();
   }
+}
 
-  function refresh() {
-    animals = [];
-    for (let i = 0; i < NUM_ANIMAL; i++) {
-      let x = random(0, width);
-      let y = random(0, height);
-      let animalImage = random(animalImgs);
-      let animal = new Animal(x, y, animalImage);
-      animals.push(animal);
-    }
-
+function refresh() {
+  animals = [];
+  for (let i = 0; i < NUM_ANIMAL; i++) {
     let x = random(0, width);
     let y = random(0, height);
-    captcha = new Captcha(x, y, captchaImg);
+    let animalImage = random(animalImgs);
+    let animal = new Animal(x, y, animalImage);
+    animals.push(animal);
   }
 
-  function test() {
-    if (clicks > 5) {
-      window.alert('WARNING : Amount of trials exceeded. Bot detected.');
+  let x = random(0, width);
+  let y = random(0, height);
+  captcha = new Captcha(x, y, captchaImg);
+}
 
-      // goToNext();
-    }
+function test() {
+  if (clicks > 5) {
+    window.alert('WARNING : Amount of trials exceeded. Bot detected.');
+    push();
+    imageMode(CENTER);
+    image(botSquad, windowWidth / 2, windowHeight / 2);
+    console.log(botSquad);
+    pop();
+    playDangerSound();
+    // goToNext();
   }
+}
+
+function playDangerSound() {
+  dangerFX.play();
+}
 
 
+// function goToNext() {
+//   state = `gotchu`
+// }
 
-  // function goToNext() {
-  //   state = `gotchu`
-  // }
-
-  // function gotchu(){
-  // window.alert('Back off');
-  // push();
-  // imageMode(CENTER);
-  // image(botSquadImg);
-  // pop();
-  // }
+// function gotchu(){
+// window.alert('Back off');
+// push();
+// imageMode(CENTER);
+// image(botSquadImg);
+// pop();
+// }
