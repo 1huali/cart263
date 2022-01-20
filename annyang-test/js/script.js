ad510484d99efb5814b7158e6200c4c68a8bc2a6
1huali <wanhuali2000@gmail.com>
2022-01-20 17:22:09 -0500

    creted folder + wrote basic program

---
+++ annyang-test/js/script.js
@@ -0,0 +1,29 @@
+/**
+annyang
+Wawa Li
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+
+function setup() {
+createCanvas(500,500);
+
+if(annyang){
+  let commands = {
+    `hello` : function(){
+      alert(`hey`)}
+    };
+annyang.addCommands(commands);
+annyang.start();
+  }
+
+
+
+function draw() {
+background(0);
+
+}
