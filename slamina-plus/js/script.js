7b03cd5ef86678a157314437fbdd26b13dad5f2b
1huali <wanhuali2000@gmail.com>
2022-01-27 18:24:55 -0500

    Modifying animals theme to pizza toppings ; no topping displays at mousePressed

--- slamina-plus/js/script.js
+++ slamina-plus/js/script.js
@@ -1,149 +1,41 @@
 /**
-Stamina
+Pizza generator
 Wawa Li
 
 Project using Annyang and responsiveVoice librairies.
 */
 
 "use strict";
-const animals = [
-  "aardvark",
-  "alligator",
-  "alpaca",
-  "antelope",
-  "ape",
-  "armadillo",
-  "baboon",
-  "badger",
-  "bat",
-  "bear",
-  "beaver",
-  "bison",
-  "boar",
-  "buffalo",
-  "bull",
-  "camel",
-  "canary",
-  "capybara",
-  "cat",
-  "chameleon",
-  "cheetah",
-  "chimpanzee",
-  "chinchilla",
-  "chipmunk",
-  "cougar",
-  "cow",
-  "coyote",
-  "crocodile",
-  "crow",
-  "deer",
-  "dingo",
-  "dog",
-  "donkey",
-  "dromedary",
-  "elephant",
-  "elk",
-  "ewe",
-  "ferret",
-  "finch",
-  "fish",
-  "fox",
-  "frog",
-  "gazelle",
-  "gila monster",
-  "giraffe",
-  "gnu",
-  "goat",
-  "gopher",
-  "gorilla",
-  "grizzly bear",
-  "ground hog",
-  "guinea pig",
-  "hamster",
-  "hedgehog",
-  "hippopotamus",
-  "hog",
-  "horse",
-  "hyena",
-  "ibex",
-  "iguana",
-  "impala",
-  "jackal",
-  "jaguar",
-  "kangaroo",
-  "koala",
-  "lamb",
-  "lemur",
-  "leopard",
-  "lion",
-  "lizard",
-  "llama",
-  "lynx",
-  "mandrill",
-  "marmoset",
-  "mink",
-  "mole",
-  "mongoose",
-  "monkey",
-  "moose",
-  "mountain goat",
-  "mouse",
-  "mule",
-  "muskrat",
-  "mustang",
-  "mynah bird",
-  "newt",
-  "ocelot",
-  "opossum",
-  "orangutan",
-  "oryx",
-  "otter",
-  "ox",
-  "panda",
-  "panther",
-  "parakeet",
-  "parrot",
-  "pig",
-  "platypus",
-  "polar bear",
-  "porcupine",
-  "porpoise",
-  "prairie dog",
-  "puma",
-  "rabbit",
-  "raccoon",
-  "ram",
-  "rat",
-  "reindeer",
-  "reptile",
-  "rhinoceros",
-  "salamander",
-  "seal",
-  "sheep",
-  "shrew",
-  "silver fox",
-  "skunk",
-  "sloth",
-  "snake",
-  "squirrel",
-  "tapir",
-  "tiger",
-  "toad",
-  "turtle",
-  "walrus",
-  "warthog",
-  "weasel",
-  "whale",
-  "wildcat",
-  "wolf",
-  "wolverine",
-  "wombat",
-  "woodchuck",
-  "yak",
-  "zebra"
+const pizzaToppings = [
+  "anchovies",
+  "artichoke",
+  "bacon",
+  "breakfast bacon",
+  "Canadian bacon",
+  "cheese",
+  "chicken",
+  "chili peppers",
+  "feta",
+  "garlic",
+  "green peppers",
+  "grilled onions",
+  "ground beef",
+  "ham",
+  "hot sauce",
+  "meatballs",
+  "mushrooms",
+  "olives",
+  "onions",
+  "pepperoni",
+  "pineapple",
+  "sausage",
+  "spinach",
+  "sun-dried tomato",
+  "tomatoes"
 ];
-let currentAnimal = ``;
-let currentAnswer = ``;
+let newToppingAdded = [];
+let chosen = false;
+let currentTopping = ``;
 
 /**
 Description of preload
@@ -159,66 +51,63 @@ annyang
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  if (annyang) {
-    let commands = {
-      'I think it is *animal': guessAnimal
-    };
-    annyang.addCommands(commands);
-    annyang.start();
+  // if (annyang) {
+  //   let commands = {
+  //     'Yes': function() {
+  //       chosen = true;
+  //     },
+  //     'No': function() {
+  //       chosen = false;
+  //     }
+  //   };
+  //
+  //   annyang.addCommands(commands);
+  //   annyang.start();
+  // }
+}
 
+
+
+//pushes the new chosen topping in list and prints in console
+// function acceptProposition() {
+//   if (chosen = true) {
+//     currentTopping = topping.push();
+//     console.log(newToppingAdded);
+//   }
+  // prints new topping to screen
+// }
+
+//displays a random topping from the array list, and reverses the string thru reversedString().
+function mousePressed() {
+  currentTopping = random(pizzaToppings);
+  text(currentTopping, width / 2, height / 2);
+  displayTopping();
+  responsiveVoice.speak(currentTopping, 'UK English Male', {
+    pitch: -5,
+    rate: 1.45
+  });
+}
+
+function displayTopping() {
   push();
-  textAlign(CENTER,CENTER);
-  textSize(4);
+  fill(255);
+  textAlign(CENTER, CENTER);
+  textSize(40);
   textStyle(BOLD);
   pop();
 }
-}
-
 
 /**
 Description of draw()
 */
 function draw() {
-background(0);
+  background(0);
 
-if (currentAnswer === currentAnimal){
-  fill(0,255,0)
-}
-  else {
-  fill(255,0,0)
-  }
-  text(currentAnswer, width/2,height/2);
-}
-
-
-
-//guesses the animal, which was stored in currentAnswer variable, and prints it in the console.
-function guessAnimal(animal) {
-  currentAnswer = animal.toLowerCase();
-    console.log(currentAnswer);
-}
-
-//chooses a random animal from the array list, and reverses the string thru reversedString().
-function mousePressed() {
-currentAnimal = random(animals);
-  let reversedAnimal = reverseString(currentAnimal);
-
-  responsiveVoice.speak(reversedAnimal, 'UK English Male', {
-  pitch:-5,
-  rate:1.45
-});
-}
-
-/**
-Reverses the provided string
-*/
-function reverseString(string) {
-  // Split the string into an array of characters
-  let characters = string.split('');
-  // Reverse the array of characters
-  let reverseCharacters = characters.reverse();
-  // Join the array of characters back into a string
-  let result = reverseCharacters.join('');
-  // Return the result
-  return result;
+  // if (chosen = true){
+  //   fill(0,255,0)
+  // }
+  //   else {
+  //   fill(255,0,0)
+  //   }
+  //   text(currentAnswer, width/2,height/2);
 }
