838c7f33dcc2af4816126939e57da3d7da4a39e8
1huali <wanhuali2000@gmail.com>
2022-02-03 08:54:52 -0500

    Prompt for name and displays

--- spy-profile/js/script.js
+++ spy-profile/js/script.js
@@ -7,6 +7,13 @@ author, and this description to match your project!
 */
 
 "use strict";
+let spyProfile = {
+  name : `redacted`,
+  alias : `redacted`,
+  secretWeapon : `redacted`,
+  password : `redacted`,
+}
+
 let currentUsername = ``;
 let password = ``;
 let secretWeapon = ``;
@@ -23,7 +30,9 @@ function preload() {
 Description of setup
 */
 function setup() {
+createCanvas(windowWidth, windowHeight);
 
+spyProfile.name = prompt(`Type your name.`)
 }
 
 
@@ -31,5 +40,12 @@ function setup() {
 Description of draw()
 */
 function draw() {
+background(255);
 
+push();
+textFont(`Courrier, monospace`);
+textSize(24);
+textAlign(TOP, LEFT);
+text(spyProfile.name, 100, 100);
+pop();
 }
