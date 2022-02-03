43eb1bb352d83dc49afc7db77603dbbdd7ace147
1huali <wanhuali2000@gmail.com>
2022-02-03 17:22:28 -0500

    Prints randomized characteristics, except from color

--- powerpuff girl-quiz-generator/js/script.js
+++ powerpuff girl-quiz-generator/js/script.js
@@ -1,5 +1,5 @@
 /**
-Title of Project
+Pet Shop - adoption center
 Author Name
 
 This is a template. You must fill in the title,
@@ -7,24 +7,33 @@ author, and this description to match your project!
 */
 
 "use strict";
-let spyProfile = {
-  name: `REDACTED`,
-  alias: `REDACTED`,
-  secretWeapon: `REDACTED`,
-  password: `REDACTED`,
-}
+// let radio = undefined;
+// let currentQuestion = `Which powerpuffGirl are U?`;
+let animalData = `undefined`;
+let colorData = `undefined`;
+let animalColor = `undefined`;
+let formData = `undefined`;
+let animal = `undefined`;
 
-let animalData = undefined;
-let instrumentData = undefined;
-let flowerData = undefined;
+let characteristic = {
+  type: `tbd`,
+  form: `tbd`,
+  element: `tbd`
+}
+//
+// let temperData = undefined;
+// let weaknessData = undefined;
+// let enjoymentData = undefined;
 
 /**
-Description of preload
+Each ppgrl has 3 characteristic
 */
 function preload() {
   animalData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/animals/common.json`);
-  instrumentData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/music/instruments.json`);
-  flowerData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/plants/flowers.json`);
+  formData = loadJSON(`assets/form.json`);
+  colorData = loadJSON (`https://raw.githubusercontent.com/dariusk/corpora/master/data/colors/wikipedia.json`);
+
+  // radio = createRadio();
 }
 
 
@@ -35,38 +44,49 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   // generateSpyProfile();
 
-  let data = JSON.parse(localStorage.getItem(`spyProfileData`));
-  // user enters the password that have been generated before, if not it generates a new profile
-  if (data !== null) {
-    let password = prompt(`Enter password`);
-    if (password === data.password) {
-    setSpyProfile();
-    }
-  } else {
-    generateSpyProfile();
-  }
+animal = random(animalData.animals);
+
+let formAnimal = random(formData.animals);
+characteristic.type = random(formAnimal.Type);
+characteristic.form = random(formAnimal.Form);
+characteristic.element = random(formAnimal.Element);
+animalColor = random(colorData.name);
+
+
+
+
+  // let data = JSON.stringify(localStorage.getItem(`spyProfileData`));
+  // // user enters the password that have been generated before, if not it generates a new profile
+  // if (data !== null) {
+  //   let password = prompt(`Enter password`);
+  //   if (password === data.password) {
+  //   setSpyProfile();
+  //   }
+  // } else {
+  //   generateSpyProfile();
+  // }
 }
 
-function setSpyProfile(){
-  spyProfile.name = data.name;
-  spyProfile.alias = data.alias;
-  spyProfile.secretWeapon = data.secretWeapon;
-  spyProfile.password = data.password;
-}
+// function setAnimalProfile(){
+//   characteristic.name = data.name;
+//   characteristic. = data.alias;
+//   characteristic. = data.secretWeapon;
+//   characteristic. = data.password;
+// }
 
 
-function generateSpyProfile() {
-  spyProfile.name = prompt(`Type your name.`)
-
-  let animals = random(animalData.animals);
-
-  spyProfile.alias = `The ${animals}`;
-  spyProfile.secretWeapon = random(instrumentData.instruments);
-  spyProfile.password = random(flowerData.flowers);
-
-  // Will save profile when the page is loaded
-  localStorage.setItem(`spyProfileData`, JSON.stringify(spyProfile));
-}
+// function generateSpyProfile() {
+//   // spyProfile.name = prompt(`Type your name.`)
+//
+//   // let animals = random(animalData.animals);
+//
+//   spyProfile.animal = `The ${animals}`;
+//   spyProfile.characteristic.form = random(instrumentData.instruments);
+//   spyProfile. = random(flowerData.flowers);
+//
+//   // Will save profile when the page is loaded
+//   localStorage.setItem(`spyProfileData`, JSON.stringify(spyProfile));
+// }
 
 /**
 Description of draw()
@@ -74,17 +94,20 @@ Description of draw()
 function draw() {
   background(255);
 
-  let profile = `CONFIDENTIAL DATA
-Name: ${spyProfile.name}
-Alias: ${spyProfile.alias}
-Secret Weapon: ${spyProfile.secretWeapon}
-Password: ${spyProfile.password}`;
+  let profile = `This is ur new friend
+Form : ${animal}
+Type : ${characteristic.type}
+Element: ${characteristic.element}
+Color : ${animalColor}
+  `;
 
   push();
-  textFont(`Courrier, monospace`);
+  // textFont(`Courrier, monospace`);
   textSize(24);
   textAlign(TOP, LEFT);
   fill(0);
   text(profile, 100, 100);
   pop();
+
+  // console.log(animalColor);
 }
