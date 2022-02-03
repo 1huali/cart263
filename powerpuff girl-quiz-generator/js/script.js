c7ea6cab529ec57499485cf40e18b416671b6362
1huali <wanhuali2000@gmail.com>
2022-02-03 18:59:37 -0500

    guardian profile generates every time
    we want to store the guardian profile for this secret. if we want tot generate a new secret, we need a new guardian. can we have a list of guardians after?

--- powerpuff girl-quiz-generator/js/script.js
+++ powerpuff girl-quiz-generator/js/script.js
@@ -14,15 +14,16 @@ let formData = `undefined`;
 let animal = `undefined`;
 let name = `undefined`;
 let animalResponse = false;
+let secretData = undefined;
 
-let mood = `undefined`;
+// let mood = `undefined`;
 let animalEcho = undefined;
 
 let characteristic = {
   type: `tbd`,
   form: `tbd`,
   element: `tbd`,
-  name:`tbd`
+  name: `tbd`
 }
 //
 // let temperData = undefined;
@@ -35,10 +36,8 @@ loads the
 function preload() {
   animalData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/animals/common.json`);
   formData = loadJSON(`assets/form.json`);
-  colorData = loadJSON (`https://raw.githubusercontent.com/dariusk/corpora/master/data/colors/wikipedia.json`);
-    animalEcho = loadSound(`assets/sounds/bark.wav`)
-
-
+  colorData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/colors/wikipedia.json`);
+  animalEcho = loadSound(`assets/sounds/bark.wav`)
 }
 
 
@@ -49,59 +48,88 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   // generateSpyProfile();
 
-animal = random(animalData.animals);
-characteristic.name = prompt(`Type your name.`)
+  animal = random(animalData.animals);
+  characteristic.name = prompt(`Type your name.`)
 
-let formAnimal = random(formData.animals);
-characteristic.type = random(formAnimal.Type);
-characteristic.form = random(formAnimal.Form);
-characteristic.element = random(formAnimal.Element);
-animalColor = JSON.parse(random(colorData.name));
-
-
-if (annyang) {
-  let commands = {
-    'I promise': function() {
-animalResponse = true;
-    }
-  };
-  annyang.addCommands(commands);
-  annyang.start();
-}
+  let formAnimal = random(formData.animals);
+  characteristic.type = random(formAnimal.Type);
+  characteristic.form = random(formAnimal.Form);
+  characteristic.element = random(formAnimal.Element);
+  animalColor = random(colorData.name);
+  // Q: not working animal color displays numerals
 
 
-  // let data = JSON.stringify(localStorage.getItem(`spyProfileData`));
-  // // user enters the password that have been generated before, if not it generates a new profile
-  // if (data !== null) {
-  //   if (password === data.password) {
-  //   setSpyProfile();
+  if (annyang) {
+    let commands = {
+      'I promise': function() {
+        animalResponse = true;
+        tellMeUrSecret();
+      }
+    };
+    annyang.addCommands(commands);
+    annyang.start();
+  }
+
+  // // user enters the name he they have chosen for their animal before, if not it generates a new profile
+  let data = localStorage.getItem(`secretData`);
+
+  // if (name !== null) {
+  //   if (name === data.secret) {
+  //   setAnimalProfile();
   //   }
   // } else {
   //   generateSpyProfile();
   // }
 }
 
-
+function tellMeUrSecret(){
+  secretData = prompt(`Tell me a secret.`)
+  let secret = JSON.stringify(localStorage.getItem(`secretData`));
+}
 // function setAnimalProfile(){
-  // characteristic.name = name;
-//   characteristic.type = data.alias;
-//   characteristic.form = data.secretWeapon;
-//   characteristic.element = data.password;
+// characteristic.name = data.name;
+//   characteristic.type = data.type;
+//   characteristic.form = data.form;
+//   characteristic.element = data.element;
 // }
 
 
 // function generateSpyProfile() {
 //
-//   // let animals = random(animalData.animals);
+// animal = random(animalData.animals);
+// characteristic.name = prompt(`Type your name.`)
 //
-//   spyProfile.animal = `The ${animals}`;
-//   spyProfile.characteristic.form = random(instrumentData.instruments);
-//   spyProfile. = random(flowerData.flowers);
-//
+// let formAnimal = random(formData.animals);
+// characteristic.type = random(formAnimal.Type);
+// characteristic.form = random(formAnimal.Form);
+// characteristic.element = random(formAnimal.Element);
+// animalColor = random(colorData.name);
+// Q: not working animal color displays numerals
+
+// how do we make the profile stay N remain??
 //   // Will save profile when the page is loaded
-//   localStorage.setItem(`spyProfileData`, JSON.stringify(spyProfile));
+//   localStorage.setItem(`guardianProfile`, JSON.stringify(characteristic));
 // }
 
+function displaySecret(){
+  push();
+  // textFont(`Courrier, monospace`);
+  textSize(12);
+  textAlign(TOP, LEFT);
+  fill(0);
+  text(secretData,100,400);
+  pop();
+}
+
+function hideSecret(){
+  push();
+  // textFont(`Courrier, monospace`);
+  textSize(12);
+  textAlign(TOP, LEFT);
+  fill(0);
+  text(`safe now`,100,400);
+  pop();
+}
 
 /**
 Description of draw()
@@ -109,26 +137,26 @@ Description of draw()
 function draw() {
   background(255);
 
-  let profile = `This is ur new friend
+  let guardian = `Hi, I am the guardian keeper of your secret.
 Name: ${characteristic.name}
 Form : ${animal}
 Type : ${characteristic.type}
 Element: ${characteristic.element}
 Color : ${animalColor}
-Now promise to take care of me forever. Say "I promise" and i'm yours.`;
+Now promise to take care of me forever. Say "I promise" and your secrets are safe with me.`;
 
-if (animalResponse=== true){
-  animalEcho.play();
-  animalResponse === false;
+  if (animalResponse === true) {
+    console.log(animalResponse);
+    // animalEcho.play();
+    animalResponse === false;
+    displaySecret();
+    // setTimeout(hideSecret,3000)
 }
 
   push();
-  // textFont(`Courrier, monospace`);
   textSize(24);
   textAlign(TOP, LEFT);
   fill(0);
-  text(profile, 100, 100);
+  text(guardian, 100, 100);
   pop();
-
-  // console.log(animalColor);
 }
