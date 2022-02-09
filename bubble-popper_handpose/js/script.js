1d689ebfa9001c40df6359eede36c209c2f928a3
1huali <wanhuali2000@gmail.com>
2022-02-09 16:54:46 -0500

    Project started

---
+++ bubble-popper_handpose/js/script.js
@@ -0,0 +1,39 @@
+/**
+Bubble popper
+Wawa Li
+
+Handpose librairy exercice - usr can pop bubbles detected with hand detection
+*/
+
+"use strict";
+let bubbles;
+let video;
+let thumb;
+let index;
+let predictions =[];
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+createCanvas(640,480);
+
+video = createCapture(VIDEO);
+capture.hide();
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
