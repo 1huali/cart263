272c95bad492e9f1621c05a92e49402b66f4418b
1huali <wanhuali2000@gmail.com>
2022-02-09 17:28:31 -0500

    pin displays

--- bubble-popper_handpose/js/script.js
+++ bubble-popper_handpose/js/script.js
@@ -12,8 +12,6 @@ let bubbles;
 let handpose = undefined;
 let video = undefined;
 //Q: could we call let video; without undefined?
-let thumb;
-let index;
 let predictions = [];
 
 
@@ -47,5 +45,28 @@ function setup() {
 Description of draw()
 */
 function draw() {
+background(0);
+
+//ensures there is something happenign when a hand is detected
+if (predictions.length > 0){
+let hand = predictions[0];
+let index = hand.annotations.indexFinger;
+// i dont udnerstand shit
+let tip = index[3];
+let base = index[0];
+let tipX = tip[0];
+let tipY = tip[1];
+let baseX = base[0];
+let baseY = base[1];
+push();
+noFill();
+stroke(255,255,255);
+strokeWeight(2);
+line(baseX,baseY,tipX,tipY);
+pop();
+push();
+
+pop();
+}
 
 }
