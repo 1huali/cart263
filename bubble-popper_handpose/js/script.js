63d05b53e1430f5d096fbd507a221a6ab69d5a38
1huali <wanhuali2000@gmail.com>
2022-02-09 17:09:56 -0500

    Video works, results generates and handpose detects hand and stores in results

--- bubble-popper_handpose/js/script.js
+++ bubble-popper_handpose/js/script.js
@@ -7,27 +7,39 @@ Handpose librairy exercice - usr can pop bubbles detected with hand detection
 
 "use strict";
 let bubbles;
-let video;
+
+// the handpose model
+let handpose = undefined;
+let video = undefined;
+//Q: could we call let video; without undefined?
 let thumb;
 let index;
-let predictions =[];
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
+let predictions = [];
 
 
 /**
 Description of setup
 */
 function setup() {
-createCanvas(640,480);
+  createCanvas(640, 480);
 
-video = createCapture(VIDEO);
-capture.hide();
+  //access user's webcam
+  video = createCapture(VIDEO);
+  video.hide();
+
+  //setting handpose model
+  // q: not calling it CONST?
+  handpose = ml5.handpose(video, {
+      flipHortizontal: true,
+    },  function() {
+      console.log(`model loaded.`)
+    });
+
+// stores hand detected as results in predictions array
+  handpose.on('predict', function(results){
+    console.log(results);
+    predictions = results;
+  });
 }
 
 
