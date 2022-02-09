dc638309954517ee5de035afa45c38592f0967cb
1huali <wanhuali2000@gmail.com>
2022-02-09 18:10:20 -0500

    Addind states (still bugging)

--- bubble-popper_handpose/js/script.js
+++ bubble-popper_handpose/js/script.js
@@ -6,6 +6,8 @@ Handpose librairy exercice - usr can pop bubbles detected with hand detection
 */
 
 "use strict";
+let state = `loading`;
+
 //the bubble
 let bubble = undefined;
 
@@ -32,6 +34,7 @@ function setup() {
     flipHortizontal: true,
   }, function() {
     console.log(`model loaded.`)
+    state === `game`;
   });
 
   // stores hand detected as results in predictions array
@@ -58,52 +61,66 @@ Description of draw()
 function draw() {
   background(0);
 
-  //ensures there is something happenign when a hand is detected
-  if (predictions.length > 0) {
-    let hand = predictions[0];
-    let index = hand.annotations.indexFinger;
-    // i dont udnerstand shit but seems to identify tipsNbase of the index representing the pin
-    let tip = index[3];
-    let base = index[0];
-    let tipX = tip[0];
-    let tipY = tip[1];
-    let baseX = base[0];
-    let baseY = base[1];
-    // body pin
+  if (state === `loading`) {
     push();
-    noFill();
-    stroke(255, 255, 255);
-    strokeWeight(2);
-    line(baseX, baseY, tipX, tipY);
+    fill(255);
+    stroke(1);
+    text(`bubbles generating`, windowWidth / 2, windowHeight / 2);
     pop();
-    // pin head
-    push();
-    noStroke();
-    fill(255, 30, 250);
-    ellipse(baseX, baseY, 20);
-    pop();
+  } else if (state === `game`) {
+    game();
+  }
 
-    // bubble checking
-    let d = dist(tipX, tipY, bubble.x, bubble.y);
-    if (d < bubble.size / 2) {
+  function game() {
+    state === `game`;
+
+    //ensures there is something happenign when a hand is detected
+    if (predictions.length > 0) {
+      let hand = predictions[0];
+      let index = hand.annotations.indexFinger;
+      // i dont udnerstand shit but seems to identify tipsNbase of the index representing the pin
+      let tip = index[3];
+      let base = index[0];
+      let tipX = tip[0];
+      let tipY = tip[1];
+      let baseX = base[0];
+      let baseY = base[1];
+      // body pin
+      push();
+      noFill();
+      stroke(255, 255, 255);
+      strokeWeight(2);
+      line(baseX, baseY, tipX, tipY);
+      pop();
+      // pin head
+      push();
+      noStroke();
+      fill(255, 30, 250);
+      ellipse(baseX, baseY, 20);
+      pop();
+
+      // bubble checking
+      let d = dist(tipX, tipY, bubble.x, bubble.y);
+      if (d < bubble.size / 2) {
+        bubble.x = random(width);
+        bubble.y = height;
+      }
+    }
+
+    // move the bubble
+    bubble.x += bubble.vx;
+    bubble.y += bubble.vy;
+
+    // reset bubble to the bottom
+    if (bubble.y < 0) {
       bubble.x = random(width);
       bubble.y = height;
     }
-  }
 
-  // move the bubble
-  bubble.x += bubble.vx;
-  bubble.y += bubble.vy;
-
-  // reset bubble to the bottom
-  if (bubble.y < 0) {
-    bubble.x = random(width);
-    bubble.y = height;
-  }
-
-  push();
-  fill(0, 100, 205);
-  noStroke();
-  ellipse(bubble.x, bubble.y, bubble.size);
-  pop();
+    push();
+    fill(0, 100, 205);
+    noStroke();
+    ellipse(bubble.x, bubble.y, bubble.size);
+    pop();
+  } // fin de state game
 }
